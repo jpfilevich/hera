8ab1ee507f3a69dc357899a811e6a0a89ee899ae
J.P. Filevich <juan.filevich@fing.edu.uy>
2017-09-02 20:39:52 -0300

    hera#download

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 /* node native */
-/* External modules aka 3rd party */
-import * as Request from "request"
-/* namespaces aka Internal modules */
+import * as fs from "fs"
+/* External modules */
+import * as request from "request"
+const progress = require('request-progress');
+/* namespaces */
 import {html, ICallback, Utils} from "./utils";
 
 export default class App {
@@ -11,7 +13,7 @@ export default class App {
     return new Promise((resolve, reject) => {
         // select http or https module, depending on reqested url
         const lib = url.startsWith('https') ? require('https') : require('http');
-        const request = lib.get(url, (response: any) => {
+        const req = lib.get(url, (response: any) => {
           // handle http errors
           if (response.statusCode < 200 || response.statusCode > 299) {
              reject(new Error('Failed to load page, status code: ' + response.statusCode));
@@ -24,7 +26,7 @@ export default class App {
           response.on('end', () => resolve(body.join('')));
         });
         // handle connection errors of the request
-        request.on('error', (err:any) => reject(err))
+        req.on('error', (err:any) => reject(err))
       })
   }
   
@@ -39,5 +41,43 @@ export default class App {
     Promise.all(promises)
       .then((html) => callback(null, html))
       .catch((err) => callback(err));
+  }
+
+  public download(urls: string | Array<string>, callback: ICallback): void {
+    if (typeof urls == "string"){
+      urls = [urls];
+    }
+    // The options argument is optional so you can omit it 
+    progress(request('https://az412801.vo.msecnd.net/vhd/VMBuild_20141027/VirtualBox/IE11/Windows/IE11.Win8.1.For.Windows.VirtualBox.zip'), {
+        // throttle: 2000,                    // Throttle the progress event to 2000ms, defaults to 1000ms 
+        // delay: 1000,                       // Only start to emit after 1000ms delay, defaults to 0ms 
+        // lengthHeader: 'x-transfer-length'  // Length header to use, defaults to content-length 
+
+    }).on('progress', function (state:any) {
+        // The state is an object that looks like this: 
+        // { 
+        //     percent: 0.5,               // Overall percent (between 0 to 1) 
+        //     speed: 554732,              // The download speed in bytes/sec 
+        //     size: { 
+        //         total: 90044871,        // The total payload size in bytes 
+        //         transferred: 27610959   // The transferred payload size in bytes 
+        //     }, 
+        //     time: { 
+        //         elapsed: 36.235,        // The total elapsed seconds since the start (3 decimals) 
+        //         remaining: 81.403       // The remaining seconds to finish (3 decimals) 
+        //     } 
+        // } 
+        console.log('progress', state);
+
+    }).on('error', function (err:any) {
+        // Do something with err 
+        console.log("error happend")
+
+    }).on('end', function () {
+        // Do something after request finishes 
+        console.log("end happend")
+        callback(null);
+
+    }).pipe(fs.createWriteStream('IE11.Win8.1.For.Windows.VirtualBox.zip'));
   }
 }
