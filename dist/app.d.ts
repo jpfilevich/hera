787901334691efc01bcab282516c21cd77a0918b
J.P. Filevich <juan.filevich@fing.edu.uy>
2017-09-02 21:04:55 -0300

    removed dist files

--- dist/app.d.ts
+++
@@ -1,6 +0,0 @@
-import { ICallback } from "./utils";
-export default class App {
-    private getContent(url);
-    get(urls: string | Array<string>, callback: ICallback): void;
-    download(urls: string | Array<string>, callback: ICallback): void;
-}
--- dist/utils.d.ts
+++
@@ -1,8 +0,0 @@
-export interface html extends String {
-}
-export interface ICallback {
-    (error: Error | null, result?: any): void;
-}
-export declare class Utils {
-    static getLink(body: html): string;
-}
