aad1ae8474755ea702d709c7b9be80629028c740
J.P. Filevich <juan.filevich@fing.edu.uy>
2017-09-03 07:48:50 -0300

    lolz

--- test/_tmp.ts
+++ test/_tmp.ts
@@ -1,9 +1,12 @@
 import Hera from "../dist/app";
 
-//let hera: Hera = new Hera();
 let urls: Array<string> = [
-    //"https://az412801.vo.msecnd.net/vhd/VMBuild_20141027/VirtualBox/IE11/Windows/IE11.Win8.1.For.Windows.VirtualBox.zip"
     "http://vshare.io/d/9ac2027",
-    "http://vshare.io/d/21969c1"
+    "http://vshare.io/d/21969c1",
+    "http://vshare.io/d/b4ecb94",
+    "http://vshare.io/d/4009078",
+    "http://vshare.io/d/c84e39b",
+    "http://vshare.io/d/d05f561",
+    "http://vshare.io/d/4f19efb"
   ];
 Hera.download(urls);
